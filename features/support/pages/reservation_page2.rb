002c7721c94abf429d7b391433170a2a02289917
Nathalia Freire <nfreire@bionexo.com>
2019-10-25 21:54:14 -0300

    Primeiro commit

--- features/support/pages/reservation_page2.rb
+++
@@ -1,9 +0,0 @@
-class ReservationPage2 < SitePrism::Page
-    set_url '/mercuryreservation2.php'
-
-    element :btn_continue, 'input[name*="reserveFlights"]'
-
-    def click_continue
-        btn_continue.click
-    end
-end
